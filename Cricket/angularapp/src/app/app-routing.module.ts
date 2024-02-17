cce0648f63819db8d3548ae5e2d1cd75b6f8d5b3
neojarvis <techadmin@iamneo.ai>
2024-02-17 16:17:45 +0000

    Manual-save-commit-17/02/2024--21:47:40

--- Cricket/angularapp/src/app/app-routing.module.ts
+++ Cricket/angularapp/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: RegistrationComponent },
+  { path: 'admin/venue/dashboard', component: AdminVenueDashboardComponent },
   { path: 'error', component: ErrorComponent, data: { message: 'Oops! Something went wrong.' }},
   { path: '**', redirectTo: '/error', pathMatch: 'full' },
 ];
