34cd25b65316b6724fd5312fd909325ee67423f6
neojarvis <techadmin@iamneo.ai>
2024-02-18 17:46:41 +0000

    Manual-save-commit-18/02/2024--23:16:38

---
+++ onlineresortbooking/angularapp/src/app/customer-view-review/customer-view-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-view-review',
+  templateUrl: './customer-view-review.component.html',
+  styleUrls: ['./customer-view-review.component.css']
+})
+export class CustomerViewReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
